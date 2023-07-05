4bd7525f53733b4ace324d02b57c831996ed6760
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-07-05 22:14:14 +0200

    feat: add example

---
+++ examples/recovery-services-vault-example/variables.tf
@@ -0,0 +1,56 @@
+variable "vault_name" {
+  description = "The name of the Recovery Services Vault which should be created."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group in which the Recovery Services Vault should be created."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure Region in which the Recovery Services Vault should be created."
+  type        = string
+}
+
+variable "sku" {
+  description = "The SKU of the Recovery Services Vault to create."
+  type        = string
+  default     = "Standard"
+}
+
+variable "public_network_access_enabled" {
+  description = "Enable public network access for the Recovery Services Vault."
+  type        = bool
+  default     = true
+}
+
+variable "immunization_enabled" {
+  description = "Enable immunization for the Recovery Services Vault."
+  type        = string
+  default     = "Locked"
+}
+
+variable "storage_model_type" {
+  description = "The storage model type of the Recovery Services Vault."
+  type        = string
+  default     = "GeoRedundant"
+}
+
+variable "soft_delete_enabled" {
+  description = "Enable soft delete for the Recovery Services Vault."
+  type        = bool
+  default     = true
+}
+
+variable "cross_region_restore_enabled" {
+  description = "Enable cross region restore for the Recovery Services Vault."
+  type        = bool
+  default     = true
+}
+
+variable "identity_type" {
+  description = "The type of identity which should be assigned to the Recovery Services Vault."
+  type        = string
+  default     = "SystemAssigned"
+}
