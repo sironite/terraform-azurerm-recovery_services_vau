0b8c09e17cc6dbd4d837649f19b2ab2180eee2dc
TheIronRock95 <58424695+TheIronRock95@users.noreply.github.com>
2023-07-05 22:10:58 +0200

    feat: module release

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vault_id" {
+  description = "The ID of the Recovery Services Vault."
+  value = azurerm_recovery_services_vault.this.id
+}
+
+output "vault_name" {
+  description = "The name of the Recovery Services Vault."
+  value = azurerm_recovery_services_vault.this.name
+}
+
+output "vault_principal_id" {
+  description = "The Principal ID of the Recovery Services Vault."
+  value = azurerm_recovery_services_vault.this.identity[0].principal_id
+}
+  
